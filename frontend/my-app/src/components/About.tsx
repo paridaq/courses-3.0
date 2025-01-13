9e54bce8b4e460fadf73aebc78874a41468e9ef7
Biswajit <paridabiswa2k@gmail.com>
2025-01-13 23:31:37 +0530

    create components

--- frontend/my-app/src/components/About.tsx
+++ frontend/my-app/src/components/About.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-        <h1>our platform provides useful content</h1>
+      <h1>our cousese are very benifitial and informative</h1>
+      <h1>buy our courses  and improve your knowledge</h1>
+        
       
     </div>
   )
