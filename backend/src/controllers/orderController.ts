23ffade42e447a2a68c7366625590f7215158f8b
Biswajit <paridabiswa2k@gmail.com>
2025-01-23 23:00:15 +0530

    adding payment method

--- backend/src/controllers/orderController.ts
+++ backend/src/controllers/orderController.ts
@@ -13,21 +13,40 @@ import productModel from "../models/productModel";
     
     
 }
+const razorpay = new Razorpay({
+    key_id: process.env.KEY_ID || '',
+    key_secret: process.env.KEY_SECRET || ''
+})
+
 
 export const createOrder = async(req:Request,res:Response):Promise<any>=>{
     const{productName,price} = req.body;
-    
-    const razorpay = new Razorpay({
-        key_id: process.env.KEY_ID || '',
-        key_secret: process.env.KEY_SECRET || ''
-    })
-
+    try{
+   
     const options = {
-        "amount":100*price,
-        "currency"
-        
+        amount:100*price,
+        currency:"INR",
+        receipt:`reciept_${Date.now()}`,
+    }
+    const order = razorpay.orders.create(options)
+    return res.status(200).send({
+        success:true,
+        message:'order created succesfully',
+        order
+    })
+    }catch(error){
+        console.log(error)
+        return res.send({
+            success:false,
+            message:'server error during creating order',
+            error
+        })
     }
 
+}
+
+export const verifyOrder = async(req:Request,res:Response):Promise<any>=>{
+    const{orderId,paymentId,signature,productName,productPrice} = req.body
 }
 
 
